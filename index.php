866e9c1a62a655ebfe3b6b01e0c5f6757ba39a8e
Tarun Saini <tsaini530@gmail.com>
2016-09-12 23:19:22 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en-IN">
+<head><title>Monarch</title>
+<link rel="stylesheet" type="text/css" href="./stylesheets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./stylesheets/styles.css">
+
+<script src="javascripts/js/bootstrap.min.js"></script>
+<script src="javascripts/main.js"></script>
+
+
+
+
+</head>
+<body  class="container">
+
+<form novalidate="false" >
+	<div class="panel">
+	<div class="panel-heading">
+		<span class="pull-left">Tin:1234567890</span>	<span class="pull-right">Mob:9928663963	</span>
+		<div class="clear" ></div>
+		<div class="form-inline pull-right form-group ">
+				<label  class="control-label" for="customer">Date: </label>
+				<input type="text" name="customer" id="customer" class="form-control" value="">
+		</div>
+		<div class="panel-title">
+		<h1 class="text-center "><strong> Monarch Art and frames</strong></h1>
+		<h3 class="text-center "><strong>P-2 AnandPuri M.D.Road Jaipur,302004</strong></h3>
+		</div>
+	</div>
+	
+	<div class="panel-body">
+	<div class="form-inline ">
+		<div class="form-group col-xs-6 " >
+			<label class="control-label "  for="customer">Name: </label>
+			<input type="text" name="customer" id="customer" class="form-control " value="">
+		</div>
+		<div class="form-group col-xs-6" >
+			<label class="control-label "  for="mobile">Mob: </label>
+			<input type="text" name="mobile" id="mobile" class="form-control " value="">
+		</div>
+	</div>
+	<div class="clear"></div>
+	<div class=" form-inline ">
+		<div class="form-group col-xs-6 " >
+			<label class="control-label "  for="qty">Qty: </label>
+			<input type="text" name="ty" id="ty" class="form-control " value="">
+		</div>
+		<div class="form-group col-xs-6" >
+			<label class="control-label "  for="price">Price: </label>
+			<input type="number" name="price" id="price" class="form-control " value="">
+		</div>
+	</div>
+	<div class="clear"></div>
+
+	<div class="form-inline ">
+		<div class="form-group col-xs-6 " >
+			<label class="control-label "  for="address">Address: </label>
+			<textarea class="form-control" name="address" id="address"></textarea> 
+		</div>
+		<div class="form-group col-xs-6" >
+			<label class="control-label "  for="code">code: </label>
+			<input type="number" name="code" id="code" class="form-control " value="">
+		</div>
+	</div>
+	<div class="text-right">
+		<button type="submit" class="btn btn-primary legitRipple">Submit form <i class="icon-arrow-right14 position-right"></i></button></div>
+</div>
+</div>
+
+</form>
+
+</body>
+</html>
